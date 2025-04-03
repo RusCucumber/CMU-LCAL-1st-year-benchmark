fb353c4c0702ab7c1aa603b21930898337d739c7
RusCucumber <w.d.ryu.m@gmail.com>
2025-04-03 14:01:04 -0400

    [upd] add apa ancestry search results

--- src/cmu_lcal_1st_year_benchmark/summarize_db_result.py
+++ src/cmu_lcal_1st_year_benchmark/summarize_db_result.py
@@ -16,6 +16,7 @@ def load_datasets(config: Config) -> Dict[str, pd.DataFrame]:
     ancestry_search_result_path = config.processed_data_dir / "ancestry_search_result.csv"
     forward_search_result_path = config.processed_data_dir / "forward_search_result.csv"
     google_scholar_result_path = config.processed_data_dir / "google_scholar_result.csv"
+    apa_ancestry_search_result_path = config.processed_data_dir / "apa_ancestry_search_result.csv"
 
     eric_result_path = config.external_data_dir / "ERIC_result.xls"
     llba_result_path = config.external_data_dir / "LLBA_result.xls"
@@ -25,6 +26,7 @@ def load_datasets(config: Config) -> Dict[str, pd.DataFrame]:
     df_ancestry_result = pd.read_csv(ancestry_search_result_path)
     df_forward_result = pd.read_csv(forward_search_result_path)
     df_google_result = pd.read_csv(google_scholar_result_path)
+    df_apa_result = pd.read_csv(apa_ancestry_search_result_path)
 
     df_eric_result = pd.read_excel(eric_result_path, engine="xlrd")
     df_llba_result = pd.read_excel(llba_result_path, engine="xlrd")
@@ -35,6 +37,7 @@ def load_datasets(config: Config) -> Dict[str, pd.DataFrame]:
         "ancestry": df_ancestry_result,
         "forward": df_forward_result,
         "google": df_google_result,
+        "apa": df_apa_result,
         "eric": df_eric_result,
         "llba": df_llba_result,
         "proquest": df_proquest_result,
@@ -76,6 +79,19 @@ def preprocess_google_scholar_dataset(df_result: pd.DataFrame) -> pd.DataFrame:
     df_result_processed.loc[:, "abstract"] = ""
     df_result_processed.loc[:, "doi"] = ""
     df_result_processed.loc[:, "document_type"] = ""
+
+    return df_result_processed
+
+def preprocess_apa_dataset(df_result: pd.DataFrame) -> pd.DataFrame:
+    # 1. remove apa
+    df_result_processed = df_result.drop("apa", axis=1)
+
+    # 2. add empty abstract & document_type
+    df_result_processed.loc[:, "abstract"] = ""
+    df_result_processed.loc[:, "document_type"] = ""
+
+    # 3. add link column
+    df_result_processed.loc[:, "link"] = "https://doi.org/" + df_result_processed["doi"]
 
     return df_result_processed
 
@@ -138,6 +154,8 @@ def preprocess(datasets: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
             df_result_processed = preprocess_semantic_scholar_dataset(df_result)
         elif dataset_name in ["google"]:
             df_result_processed = preprocess_google_scholar_dataset(df_result)
+        elif dataset_name in ["apa"]:
+            df_result_processed = preprocess_apa_dataset(df_result)
         elif dataset_name in ["eric", "llba"]:
             df_result_processed = preprocess_llba_eric_dataset(df_result)
         elif dataset_name in ["proquest"]:
@@ -163,8 +181,28 @@ def merge_datasets(datasets: Dict[str, pd.DataFrame]) -> pd.DataFrame:
     return df_result_merged
 
 def remove_doi_duplicated(df_result_merged: pd.DataFrame) -> pd.DataFrame:
-    # mask_doi_duplicate
-    pass
+    search_method_priority = {
+        "llba": 0,
+        "eric": 1,
+        "psycinfo": 2,
+        "proquest": 3,
+        "ancestry": 4,
+        "forward": 5,
+        "apa": 6,
+        "google": 7
+    }
+
+    df_result_merged_sorted = df_result_merged.sort_values(
+        "search_method",
+        key=lambda col: col.map(search_method_priority)
+    ).reset_index(drop=True)
+
+    mask_doi_duplicate = df_result_merged_sorted.duplicated(subset="doi", keep="first")
+    print(f"{mask_doi_duplicate.sum()} duplicated records were detected!")
+
+    df_result_merged_unique = df_result_merged_sorted[~mask_doi_duplicate].reset_index(drop=True)
+
+    return df_result_merged_unique
 
 def main() -> None:
     config = Config()
@@ -172,7 +210,9 @@ def main() -> None:
 
     datasets_processed = preprocess(datasets)
     df_result_merged = merge_datasets(datasets_processed)
+    df_result_merged_unique = remove_doi_duplicated(df_result_merged)
 
+    df_result_merged_unique.to_csv(config.processed_data_dir / "db_search_merged_unique.csv", index=False)
 
 if __name__ == "__main__":
     main()
